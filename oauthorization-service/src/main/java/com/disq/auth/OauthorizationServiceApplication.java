ae64b6a507ccb1dd4ffdae2498a404b1a30a474f
tarannum <tarannum.hakim@site1001.com>
2020-05-02 13:36:40 -0700

    Oautho Authorization Service

---
+++ oauthorization-service/src/main/java/com/disq/auth/OauthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.disq.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ oauthorization-service/src/test/java/com/disq/auth/OauthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disq.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
